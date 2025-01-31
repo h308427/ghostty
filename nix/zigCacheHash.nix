333e7cd95e53302a493e66f4ce237ffea79e6f18
Mitchell Hashimoto <m@mitchellh.com>
2025-01-31 12:58:01 -0800

    update zig-wayland
    This fixes a somewhat jank workaround they had to do. No functional
    changes. I also took this time to cache it in our mirror. The content
    hash always matches upstream so you can verify it yourself.

--- nix/zigCacheHash.nix
+++ nix/zigCacheHash.nix
@@ -1,3 +1,3 @@
 # This file is auto-generated! check build-support/check-zig-cache-hash.sh for
 # more details.
-"sha256-Bjy31evaKgpRX1mGwAFkai44eiiorTV1gW3VdP9Ins8="
+"sha256-MAE8r0YvF2tQZ7KakXUAANHhGt7kjRhUcrk2Xax/Caw="
--- src/build/SharedDeps.zig
+++ src/build/SharedDeps.zig
@@ -435,7 +435,7 @@ pub fn add(
                 if (self.config.x11) step.linkSystemLibrary2("X11", dynamic_link_opts);
 
                 if (self.config.wayland) {
-                    const scanner = Scanner.create(b.dependency("zig_wayland", .{}), .{
+                    const scanner = Scanner.create(b, .{
                         // We shouldn't be using getPath but we need to for now
                         // https://codeberg.org/ifreund/zig-wayland/issues/66
                         .wayland_xml = b.dependency("wayland", .{})
